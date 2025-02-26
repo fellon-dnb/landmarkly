f678eb9d7f211e9c6a52e3bdfdc90f5f38524dd4
fellon-dnb <fellon@ancevt.com>
2025-02-26 12:55:32 +0300

    init commit

---
+++ src/main/java/com/fellon/landmarkly/LandmarklyApplication.java
@@ -0,0 +1,13 @@
+package com.fellon.landmarkly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandmarklyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LandmarklyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fellon/landmarkly/LandmarklyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fellon.landmarkly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandmarklyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
